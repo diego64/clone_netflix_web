981c97bd4aa76491b5a2fa9d3caca54116281f1f
Diego Ferreira <diegoferreira1964@gmail.com>
2020-12-05 18:27:39 -0300

    Integração do projeto com a API TMDB

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import Tmdb from './Tmdb';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+
+    useEffect(() => {
+        const loadAll = async () => {
+            //Pegando a lista TOTAL
+            let list = await Tmdb.getHomeList();
+            console.log(list);
+        }
+
+        loadAll();
+    }, []);
+
+
+
+
+    return (
+        <div>
+            Clone do Netfix Web
+        </div>
+    );
+}

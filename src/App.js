f7eb6a560db20578e17ae3a9c690a5666d19ea25
Diego Ferreira <diegoferreira1964@gmail.com>
2020-12-13 16:23:51 -0300

    Iniciando a listagem do filme em destaque

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { useEffect, useState } from 'react';
 import './App.css'
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
 
     const [movieList, setMovieList] = useState([]);
+    const [featuredData, setFeaturedData] = useState(null);
 
     useEffect(() => {
         const loadAll = async () => {
             //Pegando a lista TOTAL
             let list = await Tmdb.getHomeList();
             setMovieList(list);
+
+            //Pegando o Feature
         }
 
         loadAll();
@@ -20,6 +24,11 @@ export default () => {
 
     return (
         <div className="page">
+
+        {featuredData && 
+            <featuredData item ={featuredData}/>
+        }
+            
            <section className="list">
                {movieList.map((item, key) => (
                    <div>
---
+++ src/components/FeaturedMovie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './FeaturedMovie.css';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+    return (
+        <div>
+           FILME DE DESTAQUE
+        </div>
+    );
+}

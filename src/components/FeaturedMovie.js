ae8e5ceefdf757390616a56ea4e0fca4b34ffb81
Diego Ferreira <diegoferreira1964@gmail.com>
2020-12-20 17:28:26 -0300

    Adcionando os estilos dos filmes em destaque

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,8 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './FeaturedMovie.css';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({item}) => {
+    console.log(item);
+
+    let firstDate = new Date(item.first_air_date);
+    let genres = [];
+    for(let i in item.genres) {
+        genres.push(item.genres[i].name);
+    }
+
     return (
        <section className="featured" style={{
            background: 'cover',
@@ -14,9 +23,15 @@ export default ({item}) => {
                     <div className="featured--name">{item.original_name}</div>
                         <div className="featured--info">
                             <div className="featured--points">{item.vote_average} pontos</div>
-                            <div className="featured--year">2099</div>   
+                            <div className="featured--year">{firstDate.getFullYear()}</div>   
                             <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>           
              </div>
+             <div className="featured--description">{item.overview}</div>
+             <div className="featured--buttons">
+                 <a href={`/watch/${item.id}`} className="featured--watchbutton">► Assitir</a>
+                 <a href={`/list/add/${item.id}`} className="featured--mylistbutton">+ Minha Lista</a>
+             </div>
+             <div className="featured--genres"><strong>Gêneros: </strong> {genres.join(', ')}</div>
             </div>
            </div>
        </section>

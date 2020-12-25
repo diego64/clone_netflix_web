06afdd067030a5a94b2e4c5a37e4537d88d436e9
Diego Ferreira <diegoferreira1964@gmail.com>
2020-12-25 18:05:03 -0300

    Ajustando o arquivo por conta do revet

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -40,7 +40,7 @@ export default ({title, items}) => {
             <div className="movieRow--listarea">
                 <div className="movieRow--list" style={{
                     marginLeft: scrollX,
-                    width: items.results.length *150
+                    width: items.results.length * 150
                 }}>
                     {items.results.length > 0 && items.results.map((item, key) => (
                         <div key={key} className="movieRow--item">

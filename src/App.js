f8e4e2abec0295a33b73f228b58fdaaf8ef3123e
Diego Ferreira <diegoferreira1964@gmail.com>
2020-12-24 23:36:29 -0300

    Configurando o Header da página home com o icone do usuario e da Netflix

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css'
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import FeaturedMovie from './components/FeaturedMovie';
-import Header from './components/Header/index';
+import Header from './components/Header';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
 
     const [movieList, setMovieList] = useState([]);
     const [featuredData, setFeaturedData] = useState(null);
+    const [blackHeader, setBlackHeader] = useState(false);
 
     useEffect(() => {
         const loadAll = async () => {
@@ -29,10 +30,27 @@ export default () => {
         loadAll();
     }, []);
 
+    //Adicionando o efeito de fundo preto no Header e no icone do usuario
+    useEffect(() => {
+        const scrollListener = () => {
+            if(window.scrollY > 10) {
+                setBlackHeader(true);
+            } else {
+                setBlackHeader(false);
+            }
+        }
+
+        window.addEventListener('scroll', scrollListener);
+        return () => {
+            window.removeEventListener('scroll', scrollListener);
+        }
+
+    }, []);
+
     return (
         <div className="page">
 
-            <Header />
+            <Header black={blackHeader}/>
 
         {featuredData && 
             <FeaturedMovie item ={featuredData}/>
@@ -45,6 +63,12 @@ export default () => {
                    </div>
                ))}
            </section>
+
+           <footer>
+               Feito por <span role="img" aria-label="caixa">📦</span> por Diego F.<br/>
+               Direitos de imagem para Netflix<br/>
+               Dados pegos no site Themoviedb.org
+           </footer>
         </div>
     );
 }

f41af9dccb63f25e48bee380ebceb33c4a68f2ed
Diego Ferreira <diegoferreira1964@gmail.com>
2020-12-27 17:28:40 -0300

    Configurando com o design  mobile first a pagina

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -12,6 +12,8 @@ export default ({item}) => {
         genres.push(item.genres[i].name);
     }
 
+    let description = item.overview;
+
     return (
        <section className="featured" style={{
            background: 'cover',
@@ -26,7 +28,7 @@ export default ({item}) => {
                             <div className="featured--year">{firstDate.getFullYear()}</div>   
                             <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>           
              </div>
-             <div className="featured--description">{item.overview}</div>
+             <div className="featured--description">{description}</div>
              <div className="featured--buttons">
                  <a href={`/watch/${item.id}`} className="featured--watchbutton">► Assitir</a>
                  <a href={`/list/add/${item.id}`} className="featured--mylistbutton">+ Minha Lista</a>
